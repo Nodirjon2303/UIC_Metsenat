531d11745144a64a456d8c0b3752b4a068ef418b
Your Name <you@example.com>
2022-07-17 18:44:17 +0500

    requirements.txt updated

--- UIC_Metsenat/settings.py
+++ UIC_Metsenat/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['metsenat1.uz', '127.0.0.1', '165.232.133.231']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
